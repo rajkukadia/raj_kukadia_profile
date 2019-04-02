06e672c3ecab6b4dd8d7c82e3330d365ac7eb195
Raj <rajkukadia1@gmail.com>
2019-04-01 20:24:13 -0400

    UI changes

--- webserver/src/app.js
+++ webserver/src/app.js
@@ -7,7 +7,11 @@ console.log(path.join(__dirname,'../public'));
 app.use(express.static(path.join(__dirname,'../public')));
 
 app.get('/', function(req, res){
-res.send('Hello world');
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+app.get('/about', function(req, res){
+    res.sendFile(path.join(__dirname, '../public/about.html'));
 });
 
 app.listen(port, function(){
